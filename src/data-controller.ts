d0c541a61b9d5a7ce1e63f8ae98a385fd9f34575
jcdotcom <jwexler101@gmail.com>
2024-03-01 19:55:08 -0500

    finished getByRating() and deployed to pages

--- src/data-controller.ts
+++ src/data-controller.ts
@@ -14,19 +14,29 @@ function generateProductHTML(product: Product): string {
 function renderProducts(prods: Product[]): void {
     for(let p of prods){
         const storeItem = document.createElement("div");
+        storeItem.setAttribute('class','storeItemDiv');
         storeItem.innerHTML = `${generateProductHTML(p)}`;
         const mainContainer = document.getElementById("main-container");
-        mainContainer.appendChild(storeItem);
+        mainContainer!.appendChild(storeItem);
     }
 }
 
 function getByCategory(category: string): void {
+    const mainContainer = document.getElementById("main-container");
+    while (mainContainer!.firstChild) {
+        mainContainer!.removeChild(mainContainer!.lastChild);
+      }
     const newProds: Product[] = products.filter((p)=>p.category==`${category}`);
     renderProducts(newProds);
 }
 
 function getByRating(minRating: number): void {
-    // your code
+    const mainContainer = document.getElementById("main-container");
+    while (mainContainer!.firstChild) {
+        mainContainer!.removeChild(mainContainer!.lastChild);
+      }
+    const newProds: Product[] = products.sort((i1,i2)=>i2.rating-i1.rating);
+    renderProducts(newProds);
 }
 
 export { renderProducts, getByCategory, getByRating };
